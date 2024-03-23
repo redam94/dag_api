1897a251b0b754a457d4f9c26ffdf48e473a5dac
Matthew Reda <m.reda94@gmail.com>
2024-03-23 14:41:56 -0400

    added ability to get subgraphs

--- json2dag/models/DAG.py
+++ json2dag/models/DAG.py
@@ -46,64 +46,8 @@ class Node(AbstractNode):
     def __pow__(self, other):
         return self.value ** other.value
 
-    def __radd__(self, other):
-        return other.value + self.value
-
-    def __rsub__(self, other):
-        return other.value - self.value
-
-    def __rmul__(self, other):
-        return other.value * self.value
-
-    def __rtruediv__(self, other):
-        return other.value / self.value
-
-    def __rpow__(self, other):
-        return other.value ** self.value
-
-    def __eq__(self, other):
-        return self.value == other.value
-
-    def __ne__(self, other):
-        return self.value != other.value
-
-    def __lt__(self, other):
-        return self.value < other.value
-
-    def __le__(self, other):
-        return self.value <= other.value
-
-    def __gt__(self, other):
-        return self.value > other.value
-
-    def __ge__(self, other):
-        return self.value >= other.value
-
-    def __abs__(self):
-        return abs(self.value)
-
-    def __neg__(self):
-        return -self.value
-
-    def __pos__(self):
-        return +self.value
-
-    def __invert__(self):
-        return ~self.value
-
-    def __round__(self, n):
-        return round(self.value, n)
-
-    def __floor__(self):
-        return np.floor(self.value)
-
-    def __ceil__(self):
-        return np.ceil(self.value)
-
-    def __trunc__(self):
-        return
-      
 class Edge(pyd.BaseModel):
+    
     parent: Node
     child: Node
     op: Callable= Linear()
@@ -138,8 +82,6 @@ class Edge(pyd.BaseModel):
     
     def assign(self):
         self.child.value += self.apply()
-    
-    
         
     def __hash__(self):
         return hash(f"{self.parent} {self.child}")
@@ -150,6 +92,7 @@ class DAG:
         self.nodes = nodes
         self.edges = edges
         self.model = None
+        self.trace = None
     
     def display(self):
         dg = gv.Digraph()
@@ -201,7 +144,7 @@ class DAG:
             for node in dag.nodes:
                 node.value = observations[node.name]
 
-            observation_noise = pm.HalfNormal('observation_noise', 1, dims='nodes')
+            observation_noise = pm.HalfCauchy('observation_noise',1, dims='nodes')
     
             node_model = {}
             for edge in dag.edges:
@@ -214,7 +157,7 @@ class DAG:
             i = 0
             for node in dag.nodes:
                 if node.observed:
-                    node_mean = pm.Normal(f"mu_{node.name}", mu=0, sigma=1)
+                    node_mean = pm.Normal(f"mu_{node.name}", mu=0, sigma=10)
                     pm.Normal(f"{node.name}", mu=node_model.get(node.name, 0)+node_mean, sigma=observation_noise[i], observed=observations[node.name], dims='obs')
                     i += 1
             self.model = model
@@ -232,3 +175,58 @@ class DAG:
         if not kwargs:
             return pm.summary(self.trace, var_names=[f"{edge}" for edge in self.edges if edge.child.observed]+[f'mu_{node.name}' for node in self.nodes if node.observed])
         return pm.summary(self.model, **kwargs)
+
+    def get_paths(self, start, end, nodes_checked=None):
+        paths = []
+        if nodes_checked is None:
+            nodes_checked = []
+        
+        for node in self.nodes:
+            
+            if node.name in nodes_checked:
+                continue
+            
+            if node.name == start:
+                for edge in self.get_node_edges(node.name):
+                    if edge.child.name == end or edge.parent.name == end:
+                        paths.append([edge])
+                    else:
+                        nodes_checked.append(node.name)
+                        for path in self.get_paths(edge.child.name, end, nodes_checked):
+                            paths.append([edge] + path)
+                        for path in self.get_paths(edge.parent.name, end, nodes_checked):
+                            paths.append([edge] + path)
+             
+        return paths
+    
+    def get_node_noncausal_edges(self, node_name):
+        edges = []
+        for edge in self.edges:
+            if (edge.child.name == node_name): 
+                edges.append(edge)
+        return edges
+
+    def get_node_edges(self, node_name):
+        edges = []
+        for edge in self.edges:
+            if (edge.parent.name == node_name) or (edge.child.name == node_name):
+                edges.append(edge)
+        return edges
+        
+    def get_node_causal_edges(self, node_name):
+        edges = []
+        for edge in self.edges:
+            if (edge.parent.name == node_name):
+                edges.append(edge)
+        return edges
+    
+    @staticmethod
+    def dag_from_edge_list(edge_list):
+        causes = {}
+        for edge in edge_list:
+            if edge.parent.name in causes:
+                causes[edge.parent.name].append(edge.child.name)
+            else:
+                causes[edge.parent.name] = [edge.child.name]
+        return dag_from_causes_dict(causes)
+    
